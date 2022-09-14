6df1aca55402ecf9a24dbedfdf52f69638cdcbe1
DanCan30 <danielecan3008@gmail.com>
2022-09-14 16:29:38 +0200

    Splitted routes and simplified HomeController

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', "HomeController@index")->name("guest.home");
 
-// Route::get('/comics', "ComicController@index")->name("comics.index");
+Route::get('/comics', "ComicController@index")->name("comics.index");
 
-// Route::get("/comics/{comic}", "ComicController@show")->name("comics.show");
+Route::get("/comics/create", "ComicController@create")->name("comics.create");
 
-// Route::get("/comics/create", "ComicController@create")->name("comics.create");
+Route::get("/comics/{comic}", "ComicController@show")->name("comics.show");
 
-// Route::post("/comics", "ComicController@store")->name("comics.store");
+Route::post("/comics", "ComicController@store")->name("comics.store");
 
-Route::resource("/comics", "ComicController");
+// Route::resource("/comics", "ComicController");

54574d78cc49e5b204a78f03d47b2d45de066f02
DanCan30 <danielecan3008@gmail.com>
2022-09-16 16:16:03 +0200

    Added old() method to show old values when there is an error

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -5,44 +5,55 @@
 @section("main-content")
     <h2>Add a new comic</h2>
 
-        
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-
     <form action="{{ route("comics.store") }}" method="POST">
         @csrf
         
         <label for="title-input">Title</label>
-        <input required type="text" name="title" id="title-input">
+        <input required type="text" name="title" id="title-input" value="{{ old("title", "") }}">
+        @error("title")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
 
         <label for="description-input">Description</label>
         <textarea required type="text" name="description" id="description-input">
+            {{ old("description", "") }}
         </textarea>
+        @error("description")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
         
         <label for="thumb-input">Thumb</label>
-        <input required type="text" name="thumb" id="thumb-input">
+        <input required type="text" name="thumb" id="thumb-input" value="{{ old("thumb", "") }}">
+        @error("thumb")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
         
         <label for="price-input">Price</label>
-        <input required type="text" name="price" id="price-input">
+        <input required type="text" name="price" id="price-input" value="{{ old("price", "") }}">
+        @error("price")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
         
         <label for="series-input">Series</label>
-        <input required type="text" name="series" id="series-input">
+        <input required type="text" name="series" id="series-input" value="{{ old("series", "") }}">
+        @error("series")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
         
         <label for="sale-date-input">Sale Date</label>
-        <input required type="date" name="sale_date" id="sale-date-input">
+        <input required type="date" name="sale_date" id="sale-date-input" value="{{ old("sale_date", "") }}">
+        @error("sale_date")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
         
         <label for="type-input">Type</label>
         <select required name="type" id="type-input">
             <option value="comic book">Comic book</option>
             <option value="graphic novel">Graphic novel</option>
         </select>
+        @error("type")
+            <div class="displayed-error">{{ $message }}</div>
+        @enderror
 
         <input type="submit" value="Add">
     </form>
--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -10,39 +10,39 @@
         @method("PUT")
         
         <label for="title-input">Title</label>
-        <input required type="text" name="title" id="title-input" value="{{ $comic->title }}">
+        <input required type="text" name="title" id="title-input" value="{{ old("title", $comic->title) }}">
         @error("title")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
 
         <label for="description-input">Description</label>
         <textarea required type="text" name="description" id="description-input">
-            {{ $comic->description }}
+            {{ old("description", $comic->description) }}
         </textarea>
         @error("description")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
         
         <label for="thumb-input">Thumb</label>
-        <input required type="text" name="thumb" id="thumb-input" value="{{ $comic->thumb }}">
+        <input required type="text" name="thumb" id="thumb-input" value="{{ old("thumb", $comic->thumb) }}">
         @error("thumb")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
         
         <label for="price-input">Price</label>
-        <input required type="text" name="price" id="price-input" value="{{ $comic->price }}">
+        <input required type="text" name="price" id="price-input" value="{{ old("price", $comic->price) }}">
         @error("price")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
         
         <label for="series-input">Series</label>
-        <input required type="text" name="series" id="series-input" value="{{ $comic->series }}">
+        <input required type="text" name="series" id="series-input" value="{{ old("series", $comic->series) }}">
         @error("series")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
         
         <label for="sale-date-input">Sale Date</label>
-        <input required type="date" name="sale_date" id="sale-date-input" value="{{ $comic->sale_date }}">
+        <input required type="date" name="sale_date" id="sale-date-input" value="{{ old("sale_date", $comic->sale_date) }}">
         @error("sale_date")
             <div class="displayed-error">{{ $message }}</div>
         @enderror
